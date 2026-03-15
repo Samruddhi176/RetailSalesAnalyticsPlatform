faf14464b123dc87e3120f8c297d434a7189f431
Samruddhi176 <80271508+Samruddhi176@users.noreply.github.com>
2026-03-15 17:58:42 +0530

    Update generateRetailData.py

--- Data Generator/generateRetailData.py
+++ Data Generator/generateRetailData.py
@@ -1,1 +1,19 @@
+import json
+import random
+import time
+from datetime import datetime
 
+products = ["Laptop", "Phone", "Headphones", "Tablet"]
+
+def generate_event():
+    return {
+        "transaction_id": random.randint(1000, 9999),
+        "product": random.choice(products),
+        "price": round(random.uniform(50, 2000), 2),
+        "timestamp": str(datetime.utcnow())
+    }
+
+while True:
+    event = generate_event()
+    print(json.dumps(event))
+    time.sleep(1)
